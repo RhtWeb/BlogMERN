a67a0acc1704cff4bb7fe370f48dd026ebddffa0
Rohit Singh <55020650+RhtWeb@users.noreply.github.com>
2025-03-25 20:55:51 +0530

    [Router]: Route SetUp

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,51 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+import MainLayout from './layouts/MainLayout.jsx';
+import Homepage from './routes/Homepage.jsx';
+import PostList from './routes/PostList.jsx';
+import SinglePost from './routes/SinglePost.jsx';
+import WritePage from './routes/WritePage.jsx';
+import Register from './routes/Register.jsx';
+import Login from './routes/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/posts",
+        element: <PostList />,
+      },
+      {
+        path: "/:slug",
+        element: <SinglePost />,
+      },
+      {
+        path: "/write",
+        element: <WritePage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/routes/WritePage.jsx
@@ -0,0 +1,8 @@
+
+const WritePage = () => {
+  return (
+    <div>WritePage</div>
+  )
+}
+
+export default WritePage
